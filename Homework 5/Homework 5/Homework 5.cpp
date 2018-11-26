88de4a250428040dc5931de14f5c2b55e2a6b8f9
Y S H <39716458+A1aM0@users.noreply.github.com>
2018-11-26 18:38:35 +0800

    添加第五次作业的题目

--- Homework 5/Homework 5/Homework 5.cpp
+++ Homework 5/Homework 5/Homework 5.cpp
@@ -8,6 +8,15 @@ using namespace std;
 
 int main()
 {
+	//9.6.3:
+	/*Write a program that reads a source file and writes out the names of files #included.
+	Indent file names to show files #included by included files.
+	Try this program on some real source files(to get an idea of the amount of information included).*/
+
+	//9.6.8
+	/*Modify the desk calculator so that 
+	it can be invoked from main() or from other functions 
+	as a simple function call.*/
     return 0;
 }
 
